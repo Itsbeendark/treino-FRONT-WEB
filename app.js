0916a88cd6177f6efeed2d7f395eb33753be5334
Zadayo <miguelsmoraes@hotmail.com>
2025-02-17 20:47:44 -0300

    final commit

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme')
+
+    if(document.body.classList.contains('light-theme')) {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + document.body.className);
+});
